809f1ed4a8c8ac3ea8efc607d4c01057a7ca8d81
Oleksii Patsiuk <patsyuck@ukr.net>
2021-04-18 17:08:58 +0300

    Застосовано філософію мінімалізму

--- src/main.js
+++ src/main.js
@@ -7,6 +7,73 @@ defaultModules.set(PNotifyMobile, {})
 const inputCountry = document.querySelector('input#country')
 const resultArea = document.querySelector(".result")
 
+function alertNoName() {
+    alert({
+        text: `Знайдено дуже багато країн. 
+        Будь ласка, зробіть Ваш запит більш специфічним.`
+    })
+}
+
+function alertBezimenniy(len) {
+    alert({
+        text: `Знайдено ${len} країн(и). 
+        Будь ласка, конкретизуйте Ваш запит.`
+    })
+}
+
+function alertBezNazvaniya() {
+    alert({
+        text: `Ваш запит успішно виконаний.`
+    })
+}
+
+function alertDaYaPrikaluyus() {
+    alert({
+        text: `Запит порожній або некоректний. 
+        Будь ласка, введіть правильну назву країни англійською мовою.`
+    })
+}
+
+function branch11plus() {
+    alertNoName()
+    resultArea.innerHTML = ''
+}
+
+function branch2plus(data, len) {
+    alertBezimenniy(len)
+    const countries = {items: data.map(item => item.name)}
+    resultArea.innerHTML = listMenu(countries)
+}
+
+function branch1(data) {
+    alertBezNazvaniya()
+    const lang = {items: data[0].languages.map(item => item.name)}
+    resultArea.innerHTML = `<h1>${data[0].name}</h1>
+    <div class="container">
+    <ul class="list-main">
+        <li class="item-main"><b>Capital:</b> ${data[0].capital}</li>
+        <li class="item-main"><b>Population:</b> ${data[0].population}</li>
+        <li class="item-main"><b>Languages:</b>`+ listMenu(lang) +`</li>
+    </ul>
+    <img class="flag" src="${data[0].flag}" /></div>`
+}
+
+function branch0() {
+    alertDaYaPrikaluyus()
+    resultArea.innerHTML = ''
+}
+
+function branches(data) {
+    const len = data.length
+    if (len > 10) {
+        branch11plus()
+    } else if (len > 1) {
+        branch2plus(data, len)
+    } else {
+        branch1(data)
+    }
+}
+
 function handleCountriesData() {
     fetchCountries(inputCountry.value)
     .then(response => {
@@ -14,47 +81,8 @@ function handleCountriesData() {
             return response.json()
         throw new Error("Error fetching data.")
     })
-        .then(data => {
-            const len = data.length
-            if (len > 10) {
-                alert({
-                    text: `Знайдено дуже багато країн. 
-                    Будь ласка, зробіть Ваш запит більш специфічним.`
-                })
-                resultArea.innerHTML = ''
-            } else if (len > 1) {
-                alert({
-                    text: `Знайдено ${len} країн(и). 
-                    Будь ласка, конкретизуйте Ваш запит.`
-                })
-                const countries = {items: data.map(item => item.name)}
-                resultArea.innerHTML = listMenu(countries)
-            } else {
-                alert({
-                    text: `Ваш запит успішно виконаний.`
-                })
-                const lang = {items: data[0].languages.map(item => item.name)}
-                resultArea.innerHTML = 
-                `<h1>${data[0].name}</h1>
-                <div class="container">
-                <ul class="list-main">
-                    <li class="item-main"><b>Capital:</b> ${data[0].capital}</li>
-                    <li class="item-main"><b>Population:</b> ${data[0].population}</li>
-                    <li class="item-main"><b>Languages:</b>`
-                        + listMenu(lang) +
-                    `</li>
-                </ul>
-                <img class="flag" src="${data[0].flag}" />
-                </div>`
-            }
-        })
-    .catch(err => {
-        alert({
-            text: `Запит порожній або некоректний. 
-            Будь ласка, введіть правильну назву країни англійською мовою.`
-        })
-        resultArea.innerHTML = ''
-    })
+        .then(data => {branches(data)})
+    .catch(err => {branch0()})
 }
 
 inputCountry.addEventListener('input', debounce(handleCountriesData, 500))
